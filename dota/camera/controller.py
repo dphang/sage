2efe41b40bc7d3726fc872424571f56e47e7ba66
Daniel Phang <dphang12@gmail.com>
2014-05-01 15:47:28 -0400

    Update controller

--- dota/camera/controller.py
+++ dota/camera/controller.py
@@ -6,18 +6,19 @@ controlling the camera using a series of mouse events.
 Input: JSON-encoded list of timestamps and coordinates
 """
 
+import sys, select
 import json
 import datetime as dt
 from dota_camera import DotaCamera
 
-def control_camera(camera_positions):
+def control_camera(filename):
     """
     Control the Dota 2 replay camera based on a file of camera positions.
     Press K to start.
     """
     
     # Load the positions
-    with open(camera_positions, 'r') as infile:
+    with open(filename, 'r') as infile:
         camera_positions = json.load(infile)
         
     # Load camera control module
@@ -25,23 +26,26 @@ def control_camera(camera_positions):
     
     # Go through each position
     for i, pos in enumerate(camera_positions):
+        # Break if enter is pressed
+        if sys.stdin in select.select([sys.stdin], [], [], 0)[0]:
+            line = raw_input()
+            break
         start_time = dt.datetime.now()
         # Set camera to the right position
         x = pos['x']
         y = pos['y']
-        camera.set(x, y)
-        #print x, y
-        print pos['time']
+        #camera.set(x, y)
         
-        # Wait a certain amount of time if we're not at the end
+        # Wait until the next time the camera needs to be moved
         if i != len(camera_positions) - 1:
             wait_time = camera_positions[i+1]['time'] - pos['time']
             while True:
                 diff = dt.datetime.now() - start_time
                 if (diff.microseconds / 1e6) >= wait_time:
                     break
-        
-control_camera('camera_positions.json')
+                
+if __name__ == "__main__":
+    control_camera('camera_positions.json')
         
     
     
